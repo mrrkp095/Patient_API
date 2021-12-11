065fee1f6a8c2078a21574796bff4c5ed63dc1ee
mrrkp095 <mrrkp095@gmail.com>
2021-12-11 14:34:07 +0530

    Patient API

--- app.js
+++ app.js
@@ -45,11 +45,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.static("public"));
 //To allow cross-origin requests
 app.use(cors());
+// app.use(cors({origin: true, credentials: true}));
+// app.use(function(req, res, next) {
+// 	res.header("Access-Control-Allow-Origin", "*");
+// 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+// 	res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+// 	next();
+//   });
 
 //Route Prefixes
 app.use("/", indexRouter);
--- models/AccountModel.js
+++ models/AccountModel.js
@@ -4,9 +4,9 @@ var AccountSchema = new mongoose.Schema({
 	firstName: {type: String, required: false},
 	lastName: {type: String, required: false},
 	age: {type: Number, required: false},
-	mobileNumber: {type: Number, required: false},
+	mobileNumber: {type: Number, required: true},
 	genderCd: {type: String, required: false},
-	otp:{type:String,required:false},
+	otp:{type:Number,required:false},
 	checkbox: { type: Boolean, default: false },
 }, {timestamps: true});
 
